bcf7846c6b5f588da7e17ba8473768f332b31de9
Tony <gaonelleng@gmail.com>
2024-07-18 16:28:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1
+         className="text-3xl text-center text-red-700"
+      >Welcome to vite with TailwindCSS and React</h1>
+     
+      
+    </>
+  )
+}
+
+export default App
